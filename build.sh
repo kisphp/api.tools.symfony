a21ffcef06c38bc2ca587fc54b3fb8e43a35b195
Kisphp <mariusbogdan83@gmail.com>
2016-01-20 14:45:22 +0000

    removed duplicate commands call

--- build.sh
+++ build.sh
@@ -9,17 +9,15 @@ export SYMFONY_ENV=prod
 
 if [[ "dev" == "$1" ]]; then
     $COMPOSER install
+    $PHP bin/console cache:clear -e=dev
+    $PHP bin/console cache:clear -e=test
 else
     $COMPOSER install --no-dev -o -a
 fi
 
-$PHP bin/console cache:clear -e=prod
-$PHP bin/console cache:clear -e=dev
-$PHP bin/console cache:clear -e=test
-
-rm -rf var/cache/dev
-rm -rf var/cache/prod
-rm -rf var/cache/test
+#rm -rf var/cache/dev
+#rm -rf var/cache/prod
+#rm -rf var/cache/test
 
 $NPM install
 
