4d10c3a21921459e63c1984fcd0d6b29e4daaf4b
Kisphp <mariusbogdan83@gmail.com>
2016-01-20 14:26:43 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ COMPOSER=`which composer`
 NPM=`which npm`
 PHPUNIT=`which phpunit`
 
+export SYMFONY_ENV=prod
+
+if [[ "dev" == "$1" ]]; then
+    $COMPOSER install
+else
+    $COMPOSER install --no-dev -o -a
+fi
+
 $PHP bin/console cache:clear -e=prod
 $PHP bin/console cache:clear -e=dev
 $PHP bin/console cache:clear -e=test
@@ -12,12 +20,6 @@ $PHP bin/console cache:clear -e=test
 rm -rf var/cache/dev
 rm -rf var/cache/prod
 rm -rf var/cache/test
-
-if [[ "dev" == "$1" ]]; then
-    $COMPOSER install
-else
-    $COMPOSER install --no-dev -o -a
-fi
 
 $NPM install
 
