90a9a769c136bcb36973a1a72c79c566878c5354
Bogdan Rizac <mrizac@wayfair.com>
2017-05-30 15:11:03 +0200

    added download section

---
+++ src/ApiBundle/Form/TextForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ApiBundle\Form;
+
+use ApiBundle\Transfer\ApiFormTransfer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TextForm extends AbstractType
+{
+    const VALUE_TEXT = 'Text';
+    const VALUE_DOWNLOAD = 'Download';
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(ApiFormTransfer::FIELD_SOURCE, TextareaType::class, [
+                'attr' => [
+                    'rows' => 7,
+                ],
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'form-horizontal',
+            ],
+            'data_class' => ApiFormTransfer::class,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'base64';
+    }
+}
---
+++ src/ApiBundle/Transformer/DownloadTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ApiBundle\Transformer;
+
+use ApiBundle\Decoder\DecoderInterface;
+
+class DownloadTransformer implements TransformerInterface
+{
+  /**
+   * @param DecoderInterface $decoder
+   *
+   * @return string
+   */
+  public function transform(DecoderInterface $decoder)
+  {
+    $dirPath = realpath(__DIR__ . '/../../../var/');
+    $filename = '/download-queue.txt';
+
+    if (is_file($dirPath . $filename) === false) {
+      touch($dirPath . $filename);
+    }
+
+    file_put_contents($dirPath . $filename, $decoder->getData() . "\n", FILE_APPEND | LOCK_EX);
+
+    return sprintf('Url: %s was successfully saved', $decoder->getData());
+  }
+}
